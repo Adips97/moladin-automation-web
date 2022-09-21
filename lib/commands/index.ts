1ec36012e5235a247b51ca951b96e9a51441e694
adips97 <adipurnomosidik97@gmail.com>
2022-09-21 15:51:08 +0700

    test banyak

--- lib/commands/index.ts
+++ lib/commands/index.ts
@@ -2,16 +2,6 @@ import { WebsiteElementCommand } from './website.element.command';
 import { WebsiteExpectedCommands } from './website.expected.condition.command';
 import { UtilityWebCRM } from './website.utility.commad';
 
-// const elementFinder = new WebsiteElementCommand();
-// const elementCondition = new WebsiteExpectedCommands();
-// const utilityWeb = new UtilityWebCRM();
-
-// module.exports = {
-//     elementFinder,
-//     elementCondition,
-//     utilityWeb,
-// };
-
 class commands{
     elementFinder = new WebsiteElementCommand();
     elementCondition = new WebsiteExpectedCommands();
